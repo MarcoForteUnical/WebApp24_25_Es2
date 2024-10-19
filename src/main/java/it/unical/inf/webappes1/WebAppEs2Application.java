10c528ccdb42ac16817534193eedfa64a5cabd48
Marco Forte <m.forte@evo-bi.com>
2024-10-20 01:03:10 +0200

    initial commit

---
+++ src/main/java/it/unical/inf/webappes1/WebAppEs2Application.java
@@ -0,0 +1,13 @@
+package it.unical.inf.webappes1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebAppEs2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebAppEs2Application.class, args);
+    }
+
+}
